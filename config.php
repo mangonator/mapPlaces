eab717e42fda7bfc6057ecc763ce94a99858cb09
Mikael <mikael_92@msn.com>
2018-02-13 20:17:46 +0200

    + simple php function for saving new place + config for mysql connection
    config & addNew.php for saving new place to database

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'mikaeljm_admin');
+define('DB_PASSWORD', 'XH{2x1qjjn9r');
+define('DB_NAME', 'mikaeljm_places_db');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
